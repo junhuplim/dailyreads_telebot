572be3bd07754b5ecb6dac2ae6fd1c0c66fad811
junhup <junhup.lim@u.nus.edu>
2020-11-18 22:09:17 +0800

    add subscribe and unsubscribe to bot

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cron = require("node-cron");
 const { Telegraf } = require("telegraf");
+const pullMedium = require("./article");
 const User = require("./models/User");
 const app = express();
 
@@ -9,6 +11,43 @@ const apiToken = '1472016467:AAH5F_4vfjAbknSxLLSN2HK-QwhB6vN54Ww';
 const bot = new Telegraf(apiToken);
 
 bot.start((ctx) => ctx.reply('Welcome'))
+
+cron.schedule("31 20 * * *", function () {
+    pullMedium()
+      .then(function (result) {
+        articleLink = `[TODAY'S ARTICLE ](${result.rss.channel[0].item[0].link[0]})`;
+        (async () => {
+          const users = await User.findAll({ attributes: ["chatId"] });
+          users.forEach((user) => 
+            bot.telegram.sendMessage(user.chatId, articleLink, {
+              parse_mode: "markdown",
+              disable_web_page_preview: false,
+            })
+          );
+        })();
+      })
+      .catch((err) => console.error(err));
+  });
+
+bot.hears("/subscribe", async (ctx) => {
+  try {
+    await User.create({ chatId: ctx.message.chat.id });
+    return ctx.reply("successfully subscribed! Enjoy the knowledge");
+  } catch (err) {
+    return ctx.reply("you are already subscribed!");
+  }
+});
+
+bot.hears("/unsubscribe", async (ctx) => {
+  await User.destroy({
+    where: {
+      chatId: ctx.message.chat.id,
+    },
+  });
+  return ctx.reply("successfully unsubscribed, see you again!");
+});
+
+  
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hello Friend!'))
@@ -22,6 +61,9 @@ app.use(bodyParser.json());
 
 // Endpoints
 app.get('/', (req,res) => res.send('INDEX'));
+
+// User routes
+app.use('/users', require('./routes/users'));
 
 app.post('/', (req, res) => {
      console.log(req.body);
